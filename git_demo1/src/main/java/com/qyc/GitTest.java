5acd17bdaf256042a5693564ad85dc4cd64386d7
Q3052996575 <102209015+Q3052996575@users.noreply.github.com>
2022-11-05 19:42:49 +0800

    第一次已提交

---
+++ git_demo1/src/main/java/com/qyc/GitTest.java
@@ -0,0 +1,14 @@
+package com.qyc;
+
+/**
+ * @author QYC
+ * @ClassName GitTest
+ * @description 测试Git
+ * @data 2022/11/05 19:40
+ * @Version V1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git00");
+    }
+}

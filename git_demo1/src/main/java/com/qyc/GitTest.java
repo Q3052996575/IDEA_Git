cdffd49610f964dd34ec9011dc8d933f80631250
Q3052996575 <102209015+Q3052996575@users.noreply.github.com>
2022-11-05 19:44:37 +0800

    第二次已提交

--- git_demo1/src/main/java/com/qyc/GitTest.java
+++ git_demo1/src/main/java/com/qyc/GitTest.java
@@ -10,5 +10,6 @@ package com.qyc;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git00");
+        System.out.println("hello git01");
     }
 }
